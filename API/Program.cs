005a89644a17e893280cf50c80ad1d147aa053e8
dany0830 <eodms083037@gmail.com>
2024-01-06 20:51:15 -0500

    End of section 2

---
+++ API/Program.cs
@@ -0,0 +1,49 @@
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container. (예를 들어 뭐 로그인, 사진 올리기 등등 이 여기 추가될듯 ?)
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// migration 하는 부분이고, DefaultConnection 은 내가 유동적으로 바꿀 수 있는 값. 저 값을 바꾸면 appsettings.Development.json 에 있는 DefaultConnection 도 바꿔야 함.
+builder.Services.AddDbContext<DataContext>(opt =>     // lambda expression
+{
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.UseHttpsRedirection();
+
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+
+try
+{
+    var context = services.GetRequiredService<DataContext>();
+    await context.Database.MigrateAsync();
+    await Seed.SeedData(context);
+}
+catch (Exception ex)
+{
+    var logger = services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(ex, "An error occured during migration");
+}
+
+app.Run();
